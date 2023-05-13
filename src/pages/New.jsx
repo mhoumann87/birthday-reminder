5f5d09ce518f19fed3660c55388b0f643241f944
Michael Houmann <mhoumann87@gmail.com>
2023-05-13 09:25:25 +0200

    Add form for New page

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -4,6 +4,14 @@ const New = ({ handleSubmit }) => {
       <section className='card flex'>
         <form onSubmit={handleSubmit}>
           <input type='text' name='name' id='name' placeholder='Enter Name' />
+          <input type='text' name='day' id='day' />
+          <input type='text' name='month' id='month' />
+          <input type='text' name='year' id='year' />
+          <input type='text' name='image-link' id='image-link' />
+          <input type='text' name='phone' id='phone' />
+          <input type='email' name='email' id='email' />
+          <input type='text' name='web' id='web' />
+          <input type='submit' value='Add Friend' />
         </form>
       </section>
     </main>
